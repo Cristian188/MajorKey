d93061e324f4187c2ac3bcaba8ec2a66f415fe8e
Cristian188 <cristiandominutti@gmail.com>
2021-09-02 10:03:58 -0300

    add EF and db context

---
+++ MajorKey.Insfrastructure/DAL/ApplicationContext.cs
@@ -0,0 +1,45 @@
+﻿using MajorKey.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MajorKey.Insfrastructure.DAL
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+           : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries();
+            var utcNow = System.DateTime.UtcNow;
+
+            foreach (var entry in entries)
+            {
+                if (entry.Entity is BaseEntity trackable)
+                {
+                    switch (entry.State)
+                    {
+                        case EntityState.Modified:
+                            trackable.LastModifiedDate = utcNow;
+
+                            entry.Property("CreatedDate").IsModified = false;
+                            break;
+
+                        case EntityState.Added:
+                            trackable.CreatedDate = utcNow;
+                            trackable.LastModifiedDate = utcNow;
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
