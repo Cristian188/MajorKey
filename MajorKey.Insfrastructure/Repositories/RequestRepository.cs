5eeb46c4c0c9b9e069ff34577db28d2593aeea74
Cristian188 <cristiandominutti@gmail.com>
2021-09-02 10:31:09 -0300

    add implementation request repository

---
+++ MajorKey.Insfrastructure/Repositories/RequestRepository.cs
@@ -0,0 +1,14 @@
+﻿using MajorKey.Core.Contracts.Repositories;
+using MajorKey.Core.Models.Entities;
+using MajorKey.Insfrastructure.DAL;
+
+namespace MajorKey.Insfrastructure.Repositories
+{
+    public class RequestRepository : Repository<Request>, IRequestRepository
+    {
+        public RequestRepository(ApplicationContext context)
+          : base(context)
+        {
+        }
+    }
+}
